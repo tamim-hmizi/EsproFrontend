974a19a239844494393ec5fb2dc3d0c7aa830272
amenallahsd121 <amensmida121@gmail.com>
2024-05-11 15:06:20 +0100

    Final

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -113,12 +113,9 @@ const routes: Routes = [
             { path: 'ResearchAxis/:rdiId', loadChildren: () => import('./demo/components/pages/ResearchAxis/ResearchAxis.module').then(m => m.ResearchAxisModule) },
             { path: 'dashbordRDI', loadChildren: () => import('./demo/components/pages/dashboard/dashboard.module').then(m => m.DashboardModule) },
             { path: 'Publication', loadChildren: () => import('./demo/components/pages/publication/publication.module').then(m => m.PublicationModule) },
-<<<<<<< HEAD
-=======
             { path: 'affectation',loadChildren: () =>import('../app/demo/components/pages/affectation/affectation.module').then((m) => m.AffectationModule)},
             { path: 'vacation',loadChildren: () =>import('./demo/components/pages/vacation/vacation.module').then((m) => m.VacationModule)},
             { path: 'calendar',loadChildren: () =>import('../app/demo/components/pages/calendar/calendar.module').then((m) => m.CalendarModule)},
->>>>>>> houssem
         ],
     },
     {
